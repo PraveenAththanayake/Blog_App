750f1d7514afd5aaa88aff50eee4bdd23b1f97bb
Praveen Aththanayake <aththanayakempa@gmail.com>
2024-08-15 19:56:37 +0530

    feat: Supabase setup

---
+++ lib/core/secrets/app_secrets.dart
@@ -0,0 +1,5 @@
+class AppSecrets {
+  static const supabaseUrl = 'https://wnjpupudsprqrlglfbiw.supabase.co';
+  static const supabaseAnonKey =
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InduanB1cHVkc3BycXJsZ2xmYml3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjM3MzE1MDIsImV4cCI6MjAzOTMwNzUwMn0.QqEZF86Kf7rV8r2CbTtUEjAJ3YP0KE-IW8pY897J0zI';
+}
